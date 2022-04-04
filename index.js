3bf7543263bbb44e9df0cb92f1128e53af3d8701
saba mahd <sabamahd.sm@gmail.com>
2022-04-04 19:56:49 -0400

    initial commit + some js code

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const generateSite = require('./src/generate-site');
+const path = require('path');
